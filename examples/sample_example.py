2ea5929e30667dbc866ab7add69ba5d4484fd6fe
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2023-12-14 07:25:00 -0500

    Add source examples.

---
+++ examples/sample_example.py
@@ -0,0 +1,23 @@
+from octoflow import ns
+from octoflow.model import Client
+
+client = Client()
+
+expr = client.create_experiment("sample_experiment_1")
+
+run = expr.start_run()
+
+run.log_param(ns.num_time_steps, 10)
+run.log_param(ns.num_epochs, 5)
+
+for time_step in range(1, 11):
+    time_step_val = run.log_param(ns.time_step, time_step)
+    for epoch in range(1, 6):  # goes from 1 to 5
+        epoch_step_val = run.log_param(ns.epoch, epoch, step=time_step_val)
+        run.log_metric(ns.epoch.f1_score, 5 / epoch, step=epoch_step_val)
+    __ = run.log_metric(ns.f1_score, 0.23, step=time_step_val)
+    __ = run.log_metric(ns.accuracy, 0.23, step=time_step_val)
+
+logs = run.get_logs()
+
+print(len(logs))
