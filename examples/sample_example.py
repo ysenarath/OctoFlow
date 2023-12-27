33254a1fc2899014d9a62bb39fc9e7a9fdf05f7d
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2023-12-26 22:25:03 -0500

    Exmple updates.

--- examples/sample_example.py
+++ examples/sample_example.py
@@ -13,6 +13,7 @@ for time_step in range(1, 11):
     time_step_val = run.log_param("time_step", time_step)
     for epoch in range(1, 6):
         epoch_val = run.log_param("epoch", epoch, step=time_step_val)
+        random_int_val = run.log_param("random_int", epoch, step=epoch_val)
         run.log_metric("f1_score", 1 / epoch, step=epoch_val)
         run.log_metric("accuracy", 1 / epoch, step=epoch_val)
     run.log_metrics(
@@ -26,4 +27,31 @@ for time_step in range(1, 11):
 
 logs = run.get_logs()
 
-print(expr.search_runs())
+print(logs)
+
+expression = (expr.var["num_epochs"] == 5) & (expr.var["num_time_steps"] == 10)
+
+print(expr.search_runs(expression=expression))
+
+
+run = expr.start_run()
+
+run.log_param("num_time_steps", 10)
+# run.log_param("num_epochs", 5)
+
+# for time_step in range(1, 11):
+#     time_step_val = run.log_param("time_step", time_step)
+#     for epoch in range(1, 6):
+#         epoch_val = run.log_param("epoch", epoch, step=time_step_val)
+#         random_int_val = run.log_param("random_int", epoch, step=epoch_val)
+#         run.log_metric("f1_score", 1 / epoch, step=epoch_val)
+#         run.log_metric("accuracy", 1 / epoch, step=epoch_val)
+#     run.log_metrics(
+#         {
+#             "f1_score": 0.23,
+#             "accuracy": 0.23,
+#         },
+#         step=time_step_val,
+#     )
+
+print(run.exists())
