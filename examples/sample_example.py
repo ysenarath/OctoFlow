622b0f58b91b3381139d62c866ba0abff3b51d22
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-01-07 08:17:56 -0500

    Remove commented code and add sleep function call

--- examples/sample_example.py
+++ examples/sample_example.py
@@ -6,8 +6,8 @@ from octoflow import Client
 
 database_path = Path(os.path.dirname(os.path.realpath(__file__))) / ".." / "logs" / "octoflow.db"
 
-# if database_path.exists():
-#     os.remove(database_path)
+if database_path.exists():
+    os.remove(database_path)
 
 client = Client("sqlite:///logs/octoflow.db")
 
@@ -23,9 +23,9 @@ for time_step in range(1, 11):
     for epoch in range(1, 6):
         epoch_val = run.log_param("epoch", epoch, step=time_step_val)
         random_int_val = run.log_param("random_int", epoch, step=epoch_val)
+        time.sleep(1)
         run.log_metric("f1_score", 1 / epoch, step=epoch_val)
         run.log_metric("accuracy", 1 / epoch, step=epoch_val)
-        time.sleep(5)
     run.log_metrics(
         {
             "final": {
@@ -35,20 +35,3 @@ for time_step in range(1, 11):
         },
         step=time_step_val,
     )
-
-# print(run.get_logs())
-
-# run = expr.start_run()
-
-# run.log_param("num_time_steps", 10)
-# run.log_param("num_epochs", 5)
-
-# if run.exists():
-#     run.delete()
-#     del run
-
-# run = expr.start_run()
-
-# run.log_param("num_time_steps", 10)
-# run.log_param("num_epochs", 5)
-# run.log_param("learning_rate", 1e-5)
