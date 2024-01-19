308c41ba8dd6077f8dc0ab096d776eea65be35f5
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-01-18 22:46:36 -0500

    Refactor logging module to improve code structure and readability

--- src/octoflow/logging.py
+++ src/octoflow/logging.py
@@ -3,36 +3,27 @@ from typing import Optional, Sequence, Union
 
 __all__ = [
     "get_logger",
-    "LoggingFactory",
 ]
 
 
-class LoggingFactory:
-    def __init__(
-        self,
-        name: Optional[str] = None,
-        level: Union[int, str] = logging.INFO,
-        formatter: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers: Sequence[str] = "console",
-    ) -> None:
-        logger = logging.getLogger(name)
-        logger.setLevel(level)
-        if isinstance(handlers, str):
-            handlers = (handlers,)
-        if "console" in handlers:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.DEBUG)
-            if not isinstance(formatter, logging.Formatter):
-                # Create a formatter
-                formatter = logging.Formatter(formatter)
-            # add it to the handlers
-            stream_handler.setFormatter(formatter)
-            # Add the handlers to the logger
-            logger.addHandler(stream_handler)
-        self.base_logger = logger
-
-    def __call__(self, name: Optional[str] = None) -> logging.Logger:
-        return logging.getLogger(name)
-
-
-get_logger = LoggingFactory(name=next(iter(__name__.split("."))))
+def get_logger(
+    name: Optional[str] = None,
+    level: Union[int, str] = logging.INFO,
+    formatter: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers: Sequence[str] = "console",
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if isinstance(handlers, str):
+        handlers = (handlers,)
+    if "console" in handlers:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.DEBUG)
+        if not isinstance(formatter, logging.Formatter):
+            # Create a formatter
+            formatter = logging.Formatter(formatter)
+        # add it to the handlers
+        stream_handler.setFormatter(formatter)
+        # Add the handlers to the logger
+        logger.addHandler(stream_handler)
+    return logger
