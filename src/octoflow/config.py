11bd12ed30b38d30a223720ddbe8a5ddf8ee5fa2
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-01-22 12:18:41 -0500

    Refactor ConfigWrapper and add new features to FrozenConfig

--- src/octoflow/config.py
+++ src/octoflow/config.py
@@ -28,14 +28,21 @@ class ConfigWrapper:
             raise ValueError(msg)
         self.wrapped = wrapped
         self.signature = inspect.signature(wrapped)
+        filter_keys = set()
+        for param in self.signature.parameters.values():
+            if param.kind == param.POSITIONAL_OR_KEYWORD:
+                filter_keys.add(param.name)
+        self.filter_keys = filter_keys
         self.name = name
         self.config = config
 
-    def _update_params_from_config(self, args):
-        if self.name not in self.config:
-            # function or class name or alias is not in config
-            return args
-        config = self.config[self.name]
+    def _update_params_from_config(self, args: dict) -> dict:
+        config = self.config
+        for part in self.name.split("."):
+            if part not in config:
+                # function or class name or alias is not in config
+                return args
+            config = config[part]
         # update args
         for k in config:
             if k in args:  # and args[k] is not __placeholder__
@@ -48,6 +55,7 @@ class ConfigWrapper:
         args = self.signature.bind_partial(*args, **kwargs)
         args = args.arguments
         args = self._update_params_from_config(args)
+        args = {k: v for k, v in args.items() if k in self.filter_keys and v is not inspect.Parameter.empty}
         args = self.signature.bind_partial(**args)
         args.apply_defaults()
         args = args.arguments
@@ -61,6 +69,39 @@ class FrozenConfig(Mapping):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__()
         self._data = dict(*args, **kwargs)
+        self._parent = None
+
+    def get_root(self) -> FrozenConfig:
+        """
+        Get the root config object.
+
+        Returns
+        -------
+        FrozenConfig
+            The root config object.
+        """
+        if self._parent is None:
+            # this is the root
+            return self
+        # recursively get root
+        return self._parent.get_root()
+
+    def set_parent(self, parent: FrozenConfig) -> FrozenConfig:
+        """
+        Set the parent config object.
+
+        Parameters
+        ----------
+        parent : FrozenConfig
+            The parent config object.
+
+        Returns
+        -------
+        FrozenConfig
+            The current config object.
+        """
+        self._parent = parent
+        return self
 
     def __getitem__(self, key: str) -> Any:
         value = self._data[key]
@@ -77,13 +118,16 @@ class FrozenConfig(Mapping):
                     **kwargs,
                 )
             else:
-                return FrozenConfig(value)
+                return FrozenConfig(value).set_parent(self)
+        elif isinstance(value, str):
+            return value.format(root=self.get_root(), self=self)
         return value
 
     def __getattr__(self, name: str) -> Any:
-        if name in self:
+        try:
             return self[name]
-        return None
+        except KeyError:
+            raise AttributeError(name) from None
 
     def __len__(self) -> int:
         return len(self._data)
@@ -144,3 +188,17 @@ class Config(FrozenConfig, MutableMapping):
             return wrapped(*args, **kwargs)
 
         return builder_wrapper
+
+
+config = Config({
+    "resources": {
+        "path": "~/.octoflow",
+        "cache": {
+            "path": "{root.resources.path}/cache",
+        },
+    },
+    "logging": {
+        "level": "INFO",
+        "format": "%(asctime)s %(levelname)s %(name)s %(message)s",
+    },
+})
