6d65e61634312afb498b07193bb3195d0d61861b
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-01-24 09:30:56 -0500

    Add docstrings to plugin module

--- src/octoflow/plugin.py
+++ src/octoflow/plugin.py
@@ -3,10 +3,44 @@ import importlib
 
 class Package:
     def __init__(self, name: str, modules: list):
+        """
+        Package listing all modules to expose.
+
+        Parameters
+        ----------
+        name : str
+            Name of the package.
+        modules : list
+            List of modules to expose. Each module can be a `string` or a `dict`.
+
+            If a `string`, it is the name of the module to import.
+
+            If a `dict`, with keys:
+            `name` indicating name of the module to import.
+            `package` indicating the name of the package to import the module from.
+            If not specified, the module is imported from the current package.
+        """
         self.name = name
         self.modules = modules
 
     def import_modules(self):
+        """
+        Import all modules in the package.
+
+        The modules are imported in the order they are defined in the package.
+
+        Returns
+        -------
+        None
+            The modules are imported silently. If an error occurs, it is raised.
+
+        Raises
+        ------
+        TypeError
+            If a module is not a `string` or a `dict`.
+        ImportError
+            If a module cannot be imported.
+        """
         for module in self.modules:
             if isinstance(module, str):
                 module = {"name": module}
