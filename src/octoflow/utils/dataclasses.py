4a5f4d0f2a8a133708842ad21e965b767e388797
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-04 13:24:40 -0500

    Add support for forward_refs

--- src/octoflow/utils/dataclasses.py
+++ src/octoflow/utils/dataclasses.py
@@ -2,9 +2,27 @@ from __future__ import annotations
 
 import datetime as dt
 from dataclasses import dataclass
-from typing import Any, ClassVar, Dict, List, Literal, Mapping, Optional, Self, Set, Tuple, Union, dataclass_transform
+from types import FunctionType
+from typing import (
+    Any,
+    ClassVar,
+    Dict,
+    ForwardRef,
+    List,
+    Literal,
+    Mapping,
+    Optional,
+    Self,
+    Set,
+    Tuple,
+    Type,
+    Union,
+    dataclass_transform,
+)
 
 from typing_extensions import get_args, get_origin, get_type_hints
+
+from octoflow.typing import Property
 
 
 @dataclass_transform()
@@ -12,6 +30,14 @@ class BaseModelMeta(type):
     def __new__(cls, name, bases, attrs):
         cls = super().__new__(cls, name, bases, attrs)
         return dataclass(cls)
+
+    def update_forward_refs(cls, **kwargs: Any) -> None:
+        # update forward references in all subclasses
+        for subclass in cls.__subclasses__():
+            subclass.update_forward_refs(**kwargs)
+            # update forward references in type hints
+        for name, value in get_type_hints(cls, localns=kwargs).items():
+            cls.__annotations__[name] = value
 
 
 class BaseModel(metaclass=BaseModelMeta):
@@ -32,7 +58,8 @@ def to_dict(obj: object, exclude: Optional[Set[str]] = None) -> Dict[str, Any]:
         for attr, field in get_type_hints(obj.__class__).items():
             if attr in exclude:
                 continue
-            if field is ClassVar:
+            origin = get_origin(field) or field
+            if origin is ClassVar:
                 # ignore ClassVar fields
                 continue
             value = getattr(obj, attr)
@@ -52,6 +79,16 @@ def from_dict(cls: type, data: Dict[str, Any]) -> BaseModel:
     subtypes = get_args(cls)
     if origin is None:
         origin = cls
+    if isinstance(origin, ForwardRef):
+        origin = origin._evaluate(globals(), locals(), frozenset())
+    if origin is Property:
+        if len(subtypes) == 1:
+            # subtype is the type of Property
+            return from_dict(subtypes[0], data)
+        # do not attempt to convert the data to a type of Property
+        return data
+    if origin is ClassVar and len(subtypes) == 1 and get_origin(subtypes[0]) is Property:
+        return from_dict(subtypes[0], data)
     if origin is ClassVar:
         msg = f"unsupported type: {origin}"
         raise ValueError(msg)
@@ -74,9 +111,9 @@ def from_dict(cls: type, data: Dict[str, Any]) -> BaseModel:
             value_type = type_hints[name]
             try:
                 value = from_dict(value_type, value)
+                kwargs[name] = value
             except ValueError:
-                continue
-            kwargs[name] = value
+                pass
         return origin(**kwargs)
     if issubclass(origin, Set):
         if len(subtypes) == 0:
