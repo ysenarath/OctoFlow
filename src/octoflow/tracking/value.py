35c7383c860ec500bb20d074f9e960d0a4deb3c2
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-04 13:32:15 -0500

    Add validation for value type and key in Value class

--- src/octoflow/tracking/value.py
+++ src/octoflow/tracking/value.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Dict, List, Literal, Optional, Union
 
 from octoflow.tracking.base import Base
+from octoflow.tracking.utils import validate_slug
 
 if TYPE_CHECKING:
     from octoflow.tracking.run import Run
@@ -18,3 +19,14 @@ class Value(Base):
     step: Optional[Value] = None
     type: VariableType = "unknown"
     id: Optional[int] = None
+
+    def __post_init__(self) -> None:
+        # check if value type is valid
+        if self.type not in {"unknown", "parameter", "metric"}:
+            msg = "invalid value type"
+            raise ValueError(msg)
+        # check name is valid
+        if not validate_slug(self.key):
+            msg = f"invalid key: '{self.key}'"
+            raise ValueError(msg)
+        super().__post_init__()
