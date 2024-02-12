146ce599d2ffc87ebdc6cb9bbcde43b3f9edc07d
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-02-12 13:44:39 -0500

    Update num_epochs and range in example_client.py

--- examples/example_client.py
+++ examples/example_client.py
@@ -24,26 +24,17 @@ run = expr.start_run("test_run")
 
 run.tags["octoflow.run.hash"] = "1234567890"
 
-run.log_param("num_epochs", 10)
+run.log_param("num_epochs", 5)
 
-for ll_step in range(10):
+for ll_step in range(2):
     ll_step_val = run.log_param("ll_step", ll_step)
-    run.log_metric("loss", 0.1 * ll_step, step=ll_step_val)
-    for epoch in range(10):
+    for epoch in range(5):
         epoch_val = run.log_param("epoch", epoch, step=ll_step_val)
         # accuracy
         run.log_metric("metrics.accuracy", 0.2 * epoch, step=epoch_val)
         # additional metrics
         run.log_metrics(
             {
-                "precision": {
-                    "micro": 0.3 * epoch,
-                    "macro": 0.4 * epoch,
-                },
-                "recall": {
-                    "micro": 0.5 * epoch,
-                    "macro": 0.6 * epoch,
-                },
                 "f1": {
                     "micro": 0.7 * epoch,
                     "macro": 0.8 * epoch,
@@ -54,7 +45,7 @@ for ll_step in range(10):
         )
     run.log_metric("loss", 0.1 * ll_step, step=ll_step_val)
 
-print(store.get_value_tree(run.id))
+print(run.get_value_tree())
 
 # add completed tag
 run.tags["octoflow.run.status"] = "completed"
