8c98f008160c738b6f1cddb5410be192b2cde1de
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-10 22:49:50 -0400

    Refactor example_project.py for testing out Project

--- examples/example_project.py
+++ examples/example_project.py
@@ -1,13 +1,44 @@
+import os
 import shutil
+from pathlib import Path
 
-from octoflow.project.base import Project
+import click
 
-shutil.rmtree("./examples/example_project_folder", ignore_errors=True)
+from octoflow.project import Project
+
+curr_path = Path(os.path.dirname(os.path.abspath(__file__)))
+project_path = curr_path / "project"
 
 
-def main():
-    project = Project("./examples/example_project_folder")
+@click.group()
+def cli():
+    pass
+
+
+@cli.command("clean")
+def cli_clean():
+    shutil.rmtree(project_path, ignore_errors=True)
+
+
+@cli.command()
+@click.option("--clean", is_flag=True)
+def test(clean: bool):
+    if clean:
+        shutil.rmtree(project_path, ignore_errors=True)
+    Project(project_path)
+
+
+@cli.command()
+@click.option("--clean", is_flag=True)
+def start_run(clean: bool):
+    if clean:
+        shutil.rmtree(project_path, ignore_errors=True)
+    project = Project(project_path)
+    expr = project.experiments["sentiment-analysis"]
+    with expr.start_run() as run:
+        print(run)
 
 
 if __name__ == "__main__":
-    main()
+    # run with python examples/example_project.py start_run
+    cli()
