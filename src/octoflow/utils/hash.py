585a2439c4f0c8ff1e0c882523a174a6d6ff160a
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-15 19:21:09 -0400

    Refactor hash function and add a generic update_hash method

--- src/octoflow/utils/hash.py
+++ src/octoflow/utils/hash.py
@@ -23,10 +23,41 @@ __all__ = [
 T = TypeVar("T")
 
 
+def _update_hash(
+    object_to_hash: Any,
+    m: xxhash.xxh64,
+    *,
+    mode: Union[
+        Literal["dill", "pickle", "pkl", "source", "src"],
+        callable,
+    ] = "dill",
+    version: Optional[str] = None,
+) -> None:
+    # add a sub header
+    header = f"=={type(object_to_hash)}=="
+    m.update(header.encode("utf-8"))
+    # add the version
+    version_hash = version.encode("utf-8") if version else b""
+    m.update(version_hash)
+    # add the object hash
+    if mode == "dill":
+        val_hash = dill.dumps(object_to_hash)
+    elif mode in {"pickle", "pkl"}:
+        val_hash = pickle.dumps(object_to_hash)
+    elif mode in {"source", "src"}:
+        val_hash = inspect.getsource(object_to_hash).encode("utf-8")
+    elif callable(mode):
+        val_hash = mode(object_to_hash)
+    else:
+        msg = f"invalid mode: {mode}"
+        raise ValueError(msg)
+    m.update(val_hash)
+
+
 @runtime_checkable
 class Hashable(Protocol):
-    def __octoflow_update_hash__(self, m: xxhash.xxh64):  # noqa: PLW3201
-        m.update(dill.dumps(self))
+    def _update_hash(self, m: xxhash.xxh64):
+        _update_hash(self, m)
 
 
 def hash(value: Any, *, __m: Optional[xxhash.xxh64] = None) -> str:
@@ -38,46 +69,10 @@ def hash(value: Any, *, __m: Optional[xxhash.xxh64] = None) -> str:
             m.update(x)
     except TypeError:
         if isinstance(value, Hashable):
-            value.__octoflow_update_hash__(m)
+            value._update_hash(m)
         else:
             m.update(dill.dumps(value))
     return m.hexdigest()
-
-
-class HashUpdater:
-    def __init__(
-        self,
-        mode: Union[
-            Literal["dill", "pickle", "pkl", "source", "src"],
-            callable,
-        ],
-        version: Optional[str] = None,
-        obj: Any = None,
-    ) -> None:
-        self.mode = mode
-        self.version = version
-        self.obj = obj
-
-    def __call__(self, m: xxhash.xxh64) -> None:
-        # add a sub header
-        header = f"=={type(self.obj)}=="
-        m.update(header.encode("utf-8"))
-        # add the version
-        version_hash = self.version.encode("utf-8") if self.version else b""
-        m.update(version_hash)
-        # add the object hash
-        if self.mode == "dill":
-            val_hash = dill.dumps(self.obj)
-        elif self.mode in {"pickle", "pkl"}:
-            val_hash = pickle.dumps(self.obj)
-        elif self.mode in {"source", "src"}:
-            val_hash = inspect.getsource(self.obj).encode("utf-8")
-        elif callable(self.mode):
-            val_hash = self.mode(self.obj)
-        else:
-            msg = f"invalid mode: {self.mode}"
-            raise ValueError(msg)
-        m.update(val_hash)
 
 
 def hashable(
@@ -86,9 +81,14 @@ def hashable(
         callable,
     ] = "dill",
     version: None[str] = None,
-    obj: T = None,
+    obj: Optional[T] = None,
 ) -> T:
     if obj is None:
         return functools.partial(hashable, mode, version)
-    obj.__octoflow_update_hash__ = HashUpdater(mode, version, obj)
+    obj._update_hash = functools.partial(
+        _update_hash,
+        obj,
+        mode=mode,
+        version=version,
+    )
     return obj
