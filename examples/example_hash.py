032771cbca648ad6f0bfb37b274709da493d5073
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-15 01:51:11 -0400

    Add example_hash.py with hashable decorators

---
+++ examples/example_hash.py
@@ -0,0 +1,25 @@
+from octoflow.utils import hash
+
+
+@hash.hashable("src", version="0.0.1")
+def some_callable():
+    return "hello world"
+
+
+print(hash.hash(some_callable))
+
+
+@hash.hashable("dill", version="0.0.1")
+def some_callable():
+    return "hello world"
+
+
+print(hash.hash(some_callable))
+
+
+@hash.hashable("dill", version="0.0.2")
+def some_callable():
+    return "hello world"
+
+
+print(hash.hash(some_callable))
