ac48e81396317dc3202c2a5abdc275d12cbc06b2
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-15 19:21:19 -0400

    Update hashable decorators in example_hash.py

--- examples/example_hash.py
+++ examples/example_hash.py
@@ -1,14 +1,6 @@
 from octoflow.utils import hash
 
 
-@hash.hashable("src", version="0.0.1")
-def some_callable():
-    return "hello world"
-
-
-print(hash.hash(some_callable))
-
-
 @hash.hashable("dill", version="0.0.1")
 def some_callable():
     return "hello world"
@@ -18,6 +10,22 @@ print(hash.hash(some_callable))
 
 
 @hash.hashable("dill", version="0.0.2")
+def some_callable():
+    return "hello world"
+
+
+print(hash.hash(some_callable))
+
+
+@hash.hashable("src", version="0.0.1")
+def some_callable():
+    return "hello world"
+
+
+print(hash.hash(some_callable))
+
+
+@hash.hashable("src", version="0.0.1")
 def some_callable():
     return "hello world"
 
