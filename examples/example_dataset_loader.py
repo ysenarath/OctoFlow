44771949389f1bbad3729d86faab18533e534c53
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-29 14:27:56 -0400

    Improve the dataclass based example

--- examples/example_dataset_loader.py
+++ examples/example_dataset_loader.py
@@ -1,37 +1,45 @@
+from dataclasses import field
 from typing import List, Union
 
 from octoflow.data import load_dataset, schema
-from octoflow.data.dataclass import BaseModel, field, fieldset
+from octoflow.data.dataclass import BaseModel, fields
+from octoflow.data.dataset import Dataset
 
 dset = load_dataset("jsonl", "./examples/*.jsonl")
 
 
 class Address(BaseModel):
-    street: str = field()
-    city: str = field()
-    state: str = field()
-    zip: str = field()
+    street: str
+    city: str
+    state: str
+    zip: str
 
 
 class User(BaseModel):
-    name: str = field()
-    age: Union[float, int] = field()
-    address: Address = field()
-    phone: str = field()
-    emails: List[str] = field()
+    name: str
+    age: Union[float, int]
+    address: List[Address]
+    phone: str
+    emails: List[str] = field(default_factory=list)
 
 
 s = schema.from_dataclass(User)
 
 print(s)
 
-print(
-    fieldset(User).address({
-        "address": {
+fs = fields(User)
+
+data = {
+    "address": [
+        {
             "street": "123 Main St",
             "city": "Springfield",
             "state": "IL",
             "zip": "62701",
         }
-    })
-)
+    ]
+}
+
+print(fs.address(data))
+
+print(fs.address > 0)
