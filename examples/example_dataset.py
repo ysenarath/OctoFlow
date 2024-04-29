a895ec303a08168f8a5016dd1d7bc65cf237c548
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-29 14:28:09 -0400

    Add example dataset and test data generation code

--- examples/example_dataset.py
+++ examples/example_dataset.py
@@ -1,24 +1,57 @@
+import time
+from typing import List, Union
+
+from octoflow.data.dataclass import BaseModel, field
 from octoflow.data.dataset import Dataset
 
-data = [
-    {"_id": 1, "name": "Alice", "age": 25},
-    {"_id": 2, "name": "Bob", "age": 30},
-    {"_id": 3, "name": "Charlie", "age": 35},
-    {"_id": 4, "name": "David", "age": 40},
-    {"_id": 5, "name": "Eve", "age": 45},
-    {"_id": 6, "name": "Frank", "age": 50},
-]
 
-dataset = Dataset(data).map(
-    lambda doc: {
-        "id": doc["_id"],
-        "name": doc["name"],
-        "age": doc["age"],
-    },
-    batched=True,
-    batch_size=2,
-)
+def generate_test_data():
+    for i in range(1000):
+        yield [
+            {
+                "id": f"{j}-{i}",
+                "name": "Alice",
+                "age": 30,
+                "address": [
+                    {
+                        "street": f"{i} Main St",
+                        "city": "Springfield",
+                        "state": "IL",
+                        "zip": "62701",
+                    }
+                ],
+                "phone": f"{i}{i}{i}-1233-442",
+                "emails": [],
+            }
+            for j in range(1)
+        ]
 
-docs = dataset.select("name")[0:2]
 
-print(docs)
+class Address(BaseModel):
+    street: str
+    city: str
+    state: str
+    zip: str
+
+
+class User(BaseModel):
+    name: str
+    age: Union[float, int]
+    address: List[Address]
+    phone: str
+    emails: List[str] = field(default_factory=list)
+
+
+start_time = time.time()
+dataset = Dataset(generate_test_data(), schema=User)
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Execution time with schema:", execution_time)
+
+start_time = time.time()
+dataset = Dataset(generate_test_data())
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Execution time without schema:", execution_time)
