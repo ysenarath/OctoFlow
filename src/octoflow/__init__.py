732bb4d8c67d895090c3db4f05a6d8f2a92c3a3b
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-04-29 19:38:16 -0400

    Bump version to 0.0.43

--- src/octoflow/__init__.py
+++ src/octoflow/__init__.py
@@ -5,7 +5,7 @@ from octoflow.config import config
 from octoflow.tracking import Experiment, Run, TrackingClient, Value
 from octoflow.utils.config import Config
 
-__version__ = "0.0.42"
+__version__ = "0.0.43"
 
 __all__ = [
     "Config",
