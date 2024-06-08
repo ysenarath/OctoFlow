6408cca948e374281745bd11e16b0451eb49c4c5
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-08 17:00:17 -0400

    Refactor Sampler class and add RandomSampler subclass

--- src/octoflow/data/sampler.py
+++ src/octoflow/data/sampler.py
@@ -1,32 +1,44 @@
-from typing import Mapping, Sequence
+import math
+from typing import Any, Dict, Sequence
 
 import numpy as np
+import numpy.typing as nt
 import pandas as pd
 
 __all__ = [
+    "RandomSampler",
     "Sampler",
 ]
 
 
 class Sampler:
-    def __init__(self, columns: Mapping[str, int], boundaries: bool = False):
-        if boundaries:
-            self.args = (0, *list(columns.values()), None)
-        else:
-            i = 0
-            self.args = [i]
-            for v in columns.values():
-                i += v
-                self.args.append(i)
-        self.columns = list(columns.keys())
+    def __call__(self, data: Any) -> Dict[str, Any]:
+        out = self.sample(data)
+        if isinstance(data, (pd.DataFrame, pd.Series)):
+            return {key: data.iloc[val] for key, val in out.items()}
+        return {key: data[val] for key, val in out.items()}
 
-    def __call__(self, lst: Sequence[int]):
-        lst = np.array(lst)
-        np.random.shuffle(lst)
-        return pd.Series(
-            [
-                lst[self.args[i] : self.args[i + 1]]
-                for i in range(len(self.args) - 1)
-            ],
-            index=self.columns,
-        )
+    def sample(self, data: Sequence[Any]) -> Dict[str, nt.NDArray[np.int64]]:
+        raise NotImplementedError
+
+
+class RandomSampler(Sampler):
+    def __init__(self, *args, **kwargs):
+        self.counts = dict(*args, **kwargs)
+
+    def sample(self, data: Sequence[Any]) -> Dict[str, nt.NDArray[np.int64]]:
+        n = len(data)
+        indexes = np.arange(n)
+        np.random.shuffle(indexes)
+        b = 0
+        args = [b]
+        keys = []
+        for key, size in self.counts.items():
+            if isinstance(size, float) and (0 < size <= 1):
+                size = n * size
+            b += math.floor(size)
+            args.append(b)
+            keys.append(key)
+        return {
+            key: indexes[args[i] : args[i + 1]] for i, key in enumerate(keys)
+        }
