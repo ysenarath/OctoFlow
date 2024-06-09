d4074d50c1d1d76a4ad0766cb43d2bf98379180c
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-09 12:37:29 -0400

    Refactor import statement

--- src/octoflow/core.py
+++ src/octoflow/core.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Any
 from typing_extensions import Self
 
 from octoflow.data.dataclass import BaseModel
-from octoflow.utils import objects as obj
+from octoflow.utils import objects
 
 if TYPE_CHECKING:
     from octoflow.tracking.models import Run
@@ -15,14 +15,14 @@ class Module(BaseModel):
     def get_params(self, deep: bool = True) -> dict:
         if not deep:
             raise NotImplementedError
-        return obj.dump(self)
+        return objects.dump(self)
 
     def set_params(self, **params: Any) -> Self:
         for key, value in params.items():
             if key not in self.__annotations__:
                 msg = f"invalid parameter: {key}"
                 raise ValueError(msg)
-            value = obj.load(value)
+            value = objects.load(value)
             setattr(self, key, value)
 
 
