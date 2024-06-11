7988f84c06662d03324263983afa64c6b331546b
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-10 23:33:34 -0400

    Bump version to 0.1.13

--- src/octoflow/__init__.py
+++ src/octoflow/__init__.py
@@ -5,7 +5,7 @@ from octoflow.config import config
 from octoflow.core import Module, Task
 from octoflow.utils.config import Config
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 __all__ = [
     "Config",
