9809a9c8a622fb92fc6f133d1f8f21d257e7dd1f
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2024-06-22 11:05:31 -0400

    Update version number to 0.1.16

--- src/octoflow/__init__.py
+++ src/octoflow/__init__.py
@@ -5,7 +5,7 @@ from octoflow.config import config
 from octoflow.core import Module, Task
 from octoflow.utils.config import Config
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 
 __all__ = [
     "Config",
